0628b55c8e546e7605ba5927b795c4048ecf738f
Rajesh <Rajesh@xxx.com>
2024-12-06 23:41:29 +0530

    Variable Practice

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "region_name" {}
+variable "vpc_cidr_block" {}
+variable "vpc_instance_tenancy" {}
+variable "vpc_enable_dns_hostnames" {}
+variable "vpc_enable_dns_support" {}
+variable "vpc_tags" {}
+variable "pub1_subnet_cidr_block" {}
+variable "pub1_sn_availability_zone" {}
+# variable "map_public_ip_on_launch" {}
+variable "vpc_pub1_sn_tag" {}
+variable "vpc_igw_tag" {}
+variable "rt_cidr_block" {}
+variable "Pub1_routetable_tags" {}
+variable "Sg_tag_name" {}
+variable "ec2_ami_name" {}
+variable "instance_type" {}
+variable "ec2_key_name" {}
+variable "ec2_associate_public_ip_address" {}
+variable "ec2_availability_zone" {}
+variable "ec2_name_tag" {}
+
